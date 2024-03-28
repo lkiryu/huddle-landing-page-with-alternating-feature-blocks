124a316f44e8b195cd158de408c5c5ef41a73ecc
Kiryu <13matheusilva@gmail.com>
2024-03-28 15:32:47 -0300

    section component add

--- src/components/huddleLandingPage/huddleLandingPage.js
+++ src/components/huddleLandingPage/huddleLandingPage.js
@@ -1,39 +1,14 @@
 import React from "react";
 import Header from "../header/header";
+import Section from "../section/section";
 
 
 const HuddleLandingPage = () => {
     return (
         <>
-            <Header/>
+            <Header />
 
-            <section className="section">
-                <div className="card">
-                    <div className="card-info">
-                        <h2 className="card-title"></h2>
-                        <p className="card-description"></p>
-                    </div>
-                    <img src='' alt="illustration"></img>
-                </div>
-                <div className="card">
-                    <div className="card-info">
-                        <h2 className="card-title"></h2>
-                        <p className="card-description"></p>
-                    </div>
-                    <img src='' alt="illustration"></img>
-                </div>
-                <div className="card">
-                    <div className="card-info">
-                        <h2 className="card-title"></h2>
-                        <p className="card-description"></p>
-                    </div>
-                    <img src='' alt="illustration"></img>
-                </div>
-            </section>
-            <div className="ready-to-build">
-                <h2></h2>
-                <a className="button" href="/"></a>
-            </div>
+            <Section/>
             <footer className="footer">
                 <a href="/">
                     <img src='' alt="logo"></img>
---
+++ src/components/section/section.js
@@ -0,0 +1,41 @@
+import React from "react";
+import growTogether from '../../images/illustration-grow-together.svg'
+import flowingConversations from '../../images/illustration-flowing-conversation.svg'
+import yourUsers from '../../images/illustration-your-users.svg'
+
+const Section = () => {
+    return (
+        <section className="section">
+            <div className="card">
+                <div className="card-info">
+                    <h2 className="card-title">Grow Together</h2>
+                    <p className="card-description">Generate meaningful discussions with your audience and build a strong, loyal community.
+                        Think of the insightful conversations you miss out on with a feedback form.</p>
+                </div>
+                <img src={growTogether} alt="grow-together"></img>
+            </div>
+            <div className="card">
+                <div className="card-info">
+                    <h2 className="card-title">Flowing Conversations</h2>
+                    <p className="card-description">You wouldn't paginate a conversation in real life, so why do it online? Our threads
+                        have just-in-time loading for a more natural flow.</p>
+                </div>
+                <img src={flowingConversations} alt="flowing-conversations"></img>
+            </div>
+            <div className="card">
+                <div className="card-info">
+                    <h2 className="card-title">Your Users</h2>
+                    <p className="card-description">It takes no time at all to integrate Huddle with your app's authentication solution.
+                        This means, once signed in to your app, your users can start chatting immediately.</p>
+                </div>
+                <img src={yourUsers} alt="your-users"></img>
+            </div>
+            <div className="ready-to-build">
+                <h2>Ready To Build Your Community?</h2>
+                <a className="button" href="/">Get Started For Free</a>
+            </div>
+        </section>
+    )
+}
+
+export default Section
